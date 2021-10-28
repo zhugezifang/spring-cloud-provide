1b0701da34eb031f6d8a7723098619c00b140d91
duanxiaoqiu <duanxiaoqiu@qiyi.com>
2021-10-28 16:40:41 +0800

    first

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/discovery")
+    public String discovery() {
+        return "discovery";
+    }
+
+}
