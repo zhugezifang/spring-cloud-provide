8118583992e3468955263abe8edab84621f0260a
duanxiaoqiu <duanxiaoqiu@qiyi.com>
2021-10-29 11:37:45 +0800

    修改配置

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,12 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @RequestMapping("/discovery")
+    @GetMapping("/discovery")
     public String discovery() {
         return "discovery";
     }
